ccfbf67a23048088ac0e3c86926f3cd6a3d37617
toadkarter <vrakhmanin1@gmail.com>
2023-12-28 23:49:30 +0000

    Initial movement code finished

--- Scenes/Player/player.gd
+++ Scenes/Player/player.gd
@@ -1,15 +1,23 @@
 extends CharacterBody2D
 
-@export var speed: float = 150.0
+@export var speed: float = 100.0
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 
 
 func _physics_process(delta):
+	var previous_horizontal: float = velocity.x
+	var previous_vertical: float = velocity.y
+	
 	var horizontal_direction = Input.get_axis("left", "right")
 	var vertical_direction = Input.get_axis("up", "down")
 	
-	_handle_vertical_movement(vertical_direction)
-	_handle_horizontal_movement(horizontal_direction)
+	if previous_vertical != 0.0:
+		_handle_vertical_movement(vertical_direction)
+	elif previous_horizontal != 0.0:
+		_handle_horizontal_movement(horizontal_direction)
+	else:
+		_handle_vertical_movement(vertical_direction)
+		_handle_horizontal_movement(horizontal_direction)
 
 	move_and_slide()
 	
@@ -32,7 +40,20 @@ func _handle_vertical_movement(current_direction: float) -> void:
 		
 		
 func _handle_horizontal_movement(current_direction: float) -> void:
+	var previous_velocity: float = velocity.x
+	
 	if current_direction:
 		velocity.x = current_direction * speed
+		if velocity.x != 0:
+			animated_sprite.play("side")
+			_set_flip_orientation()
 	else:
 		velocity.x = 0 
+		animated_sprite.play("side_idle")
+		
+		
+func _set_flip_orientation() -> void:
+	if velocity.x > 0 and !animated_sprite.flip_h:
+		animated_sprite.flip_h = true
+	elif velocity.x < 0 and animated_sprite.flip_h:
+		animated_sprite.flip_h = false
