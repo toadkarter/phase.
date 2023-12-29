84731e8ec1c04850b1448bf66391b52355e4a424
toadkarter <vrakhmanin1@gmail.com>
2023-12-29 08:35:09 +0000

    Adding color filter when entering the second world

---
+++ Scenes/Level/level.gd
@@ -0,0 +1,31 @@
+extends Node2D
+class_name Level
+
+@export var orbs: Array[Orb]
+
+var is_in_first_world: bool = true
+
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+
+func _ready():	
+	if orbs.is_empty():
+		print("Error: You forgot to add orbs to the level")
+		
+	for orb in orbs:
+		if orb != null:
+			orb.connect("changed_worlds", _on_changed_worlds)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+	
+	
+func _on_changed_worlds() -> void:
+	if is_in_first_world:
+		animation_player.play("start_second_world")
+		is_in_first_world = false
+	else:
+		animation_player.play_backwards()
+		is_in_first_world = true
+	
